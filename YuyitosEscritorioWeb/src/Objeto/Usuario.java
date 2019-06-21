28a9e0c728cc2c8f99afe65d357f79ea769e48c0
Sebastian Castro Contreras <33074338+scastroc@users.noreply.github.com>
2019-06-21 13:00:01 -0400

    Segundo Commit

--- YuyitosEscritorioWeb/src/Objeto/Usuario.java
+++ YuyitosEscritorioWeb/src/Objeto/Usuario.java
@@ -15,7 +15,44 @@ public class Usuario {
     private String nombre;
     private String password;
     private Perfil perfil;
-    private char estado;
+    //private char estado;
+
+    public Usuario() {
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
     
     
     
